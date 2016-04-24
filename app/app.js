21f7dad7c3ccdeeb62a500877458e6f3540b297f
MorayM <moray.macdonald1@gmail.com>
2016-04-24 18:42:58 +0100

    Added local storage, creation/deletion of plants

--- app/app.js
+++ app/app.js
@@ -1,17 +1,60 @@
-(function() {
-    
 var data = {
     allPlants: [],
     allWorkers: [],
     allGardens: []
 };  
 
-var plantRow = '<tr><td style="font-style: italic">{{plant.latin}}</td><td>{{plant.common}}</td><td>{{plant.spread}}</td><td>{{plant.height}}</td><td>{{plant.count}}</td><td></td></tr>';
+var pt;
 
-$(document).ready(function(){    
-    function addNewPlant() {
-        data.allPlants.push(vm.newPlant);
-        newPlant = {};
+$(document).ready(function(){
+    
+    if (localStorage.getItem('gardenData')) {
+        console.log(localStorage.getItem('gardenData'));
+        data = JSON.parse(localStorage.getItem('gardenData'));
     }
+    
+    pt = $('#allPlants').DataTable({
+        'data': data.allPlants,
+        'columns': [
+            {'className': 'italic'},
+            null,
+            null,
+            null,
+            null,
+            {'orderable': false}
+        ]
+    })
+    
+    $('#addPlant').unbind('click').on('click', addNewPlant);
+    
+    $('#allPlants tbody').unbind('click').on('click', 'button.deleteRow', removeRow);
+    
+    function addNewPlant() {
+        data.allPlants.push([
+            $('#latin').val(),
+            $('#common').val(),
+            parseFloat($('#spread').val()),
+            parseFloat($('#height').val()),
+            0,
+            '<button class="deleteRow">x</button>'
+        ]);
+        
+        pt.row.add([$('#latin').val(), $('#common').val(), parseFloat($('#spread').val()), parseFloat($('#height').val()), 0, '<button class="deleteRow">x</button>']).draw();
+    }
+    
+    function removeRow() {
+        var row = pt.row($(this).parents('tr')).remove().draw();
+        
+        for (var i = 0; i < data.allPlants.length; i++) {
+            if (data.allPlants[i][0] === $(this).parents('tr').children().first().text()) {
+                data.allPlants.splice(i, 1);
+            }
+        }
+    }
+    
+    var intervalId = setInterval(function() {
+        localStorage.setItem('gardenData', JSON.stringify(data));
+        console.log('saved');
+    }, 5000);
+    console.log(intervalId);
 });
-})();
